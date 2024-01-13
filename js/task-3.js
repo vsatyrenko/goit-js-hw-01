a2394a78c09fc15a2b3dcb4b9587c0b315fbc901
vsatyrenko <wsatyrenko@gmail.com>
2024-01-13 13:10:11 -0600

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -7,6 +7,6 @@ function getElementWidth(content, padding, border) {
   return (total = content + pad + bor);
 }
 
-// console.log(getElementWidth("50px", "8px", "4px"));
-// console.log(getElementWidth("60px", "12px", "8.5px"));
-// console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
