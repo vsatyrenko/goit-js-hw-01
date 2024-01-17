f88944d418e3e28eaa7b712d28ae42ac5f5e6826
vsatyrenko <wsatyrenko@gmail.com>
2024-01-17 13:54:24 -0600

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -2,6 +2,6 @@ function makeTransaction(quantity, totalPrice) {
   const sum = quantity * totalPrice;
   return `"You ordered ${quantity} droids worth ${sum} credits!"`;
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
